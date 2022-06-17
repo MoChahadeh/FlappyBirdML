27b379c92849caca6d9ef29cb4add2d6d0da3083
Mo Chahadeh <applednd2002@gmail.com>
2022-06-17 06:26:29 +0300

    turned softmax to sigmoid, rewrote forward syntax

--- neuralnet.py
+++ neuralnet.py
@@ -1,42 +1,43 @@
 import numpy as np
+from math import exp
 
+from regex import P
 
 def ReLU(Z):
 
-    return np.maximum(Z, 0)
+    return np.maximum(0, Z )
 
-def softmax(Z):
+def sigmoid(Z):
 
-    return np.exp(Z) / np.sum(np.exp(Z))
+    return 1/(1+np.exp(-Z))
 
 class NeuralNet():
 
-    def __init__(self, i, h, o):
+    def __init__(self, i:int, h:int, o:int):
 
-        self.W1 = np.random.rand(h, i) - 0.5
-        self.b1 = np.random.rand(h, 1) - 0.5
-        self.W2 = np.random.rand(o, h) - 0.5
-        self.b2 = np.random.rand(o, 1) - 0.5
+        self.W1:np.ndarray = np.random.rand(h, i) - 0.5
+        self.b1:np.ndarray = np.random.rand(h, 1) - 0.5
+        self.W2:np.ndarray = np.random.rand(o, h) -0.5
+        self.b2:np.ndarray = np.random.rand(o,1) -0.5
     
-    def forward(self, inputs: list):
+    def forward(self, inputs: np.ndarray):
 
-        Z1 = self.W1.dot(inputs) + self.b1
+        Z1 = np.dot(self.W1,np.transpose(inputs)) + self.b1
         A1 = ReLU(Z1)
 
-        Z2 = self.W2.dot(A1) + self.b2
-        A2 = softmax(Z2)
+        Z2 = np.dot(self.W2, A1) + self.b2
+        A2 = sigmoid(Z2)
 
         return A2
     
     def mutate(self, rate):
         W1r, W1c = self.W1.shape
-        self.W1 = self.W1 + (self.W1 * (np.random.randn(W1r, W1c) - 0.5) / (0.5/rate))
+        self.W1 = self.W1 + (self.W1 * (np.random.rand(W1r, W1c) - 0.5) / (0.5/rate))
         b1r, b1c = self.b1.shape
-        self.b1 = self.b1 + (self.b1 * (np.random.randn(b1r, b1c) - 0.5) / (0.5/rate))
+        self.b1 = self.b1 + (self.b1 * (np.random.rand(b1r, b1c) - 0.5) / (0.5/rate))
         W2r, W2c = self.W2.shape
-        self.W2 = self.W2 + (self.W2 * (np.random.randn(W2r, W2c) - 0.5) / (0.5/rate))
+        self.W2 = self.W2 + (self.W2 * (np.random.rand(W2r, W2c) - 0.5) / (0.5/rate))
         b2r, b2c = self.b2.shape
-        self.b2 = self.b2 + (self.b2 * (np.random.randn(b2r, b2c) - 0.5) / (0.5/rate))
-
+        self.b2 = self.b2 + (self.b2 * (np.random.rand(b2r, b2c) - 0.5) / (0.5/rate))
 
 
