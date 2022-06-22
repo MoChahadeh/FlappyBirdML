7cb96b519c4dc3ec2b048b846d085dd11992c2d6
Mo Chahadeh <applednd2002@gmail.com>
2022-06-22 03:06:42 +0300

    Removed Magic numbers + better obstacle generating logic

--- obstacle.py
+++ obstacle.py
@@ -15,9 +15,9 @@ class Obstacle(pygame.sprite.Sprite):
     def __init__(self, *groups, initX) -> None:
         super().__init__(*groups)   # superclass constructor
 
-        self.pos = pygame.Vector2(initX, randint(100, 280))     # inital position of obstacle with random y for lower rectangle
-        self.rect = pygame.rect.Rect(self.pos.x, self.pos.y, 40, 375-self.pos.y)    # lower rectangle with height spanning from pos.y to bottom of screen
-        self.upperRect = pygame.rect.Rect(self.pos.x, 0, 40, self.pos.y-randint(60,120))    # upper rectange set at y=0 and spanning down to lower rect.y - a random number between 60 and 120 pixels..
+        self.pos = pygame.Vector2(initX, randint(int(HEIGHT/4), HEIGHT-100))     # inital position of obstacle with random y for lower rectangle
+        self.rect = pygame.rect.Rect(self.pos.x, self.pos.y, 40, HEIGHT-self.pos.y+100)    # lower rectangle with height spanning from pos.y to bottom of screen
+        self.upperRect = pygame.rect.Rect(self.pos.x, 0, 40, self.pos.y-randint(minOpening,minOpening*2))    # upper rectange set at y=0 and spanning down to lower rect.y - a random number between 60 and 120 pixels..
         self.openingPos = pygame.Vector2(self.pos.x, (self.rect.top+self.upperRect.bottom)/2)   # getting the middle point of the opening of the obstacle, this is fed into the nerual net
         self.color = (70,255,100)   # color of obstacle, light green
         self.dead = False   # death state of obstacle
@@ -38,5 +38,5 @@ class Obstacle(pygame.sprite.Sprite):
 
             if(self.pos.x < -40):   # if obstacle passes the left of the screen..
                 self.dead = True    # sets dead variable to true, disabling the update method...
-                obstacles.add(Obstacle(initX = WIDTH + (150*int(genPopulation/2))))
+                obstacles.add(Obstacle(initX = obstacles.sprites()[len(obstacles.sprites())-1].pos.x + 170))
                 self.kill()     # remove the obstacle from every sprite group it's in..
